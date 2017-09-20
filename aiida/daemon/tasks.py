8297edff7c0831cbf320c0146153e78f139e428d
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2017-09-21 00:05:59 +0200

    Adding RabbitMQ as broker

--- aiida/daemon/tasks.py
+++ aiida/daemon/tasks.py
@@ -35,20 +35,9 @@ config = get_profile_config(settings.AIIDADB_PROFILE)
 engine = config["AIIDADB_ENGINE"]
 
 # defining the broker.
-# So far I am using SA for that, but we should totally
-# think about using redis or rabbitmq, which is now possibly
-# since now it is decoupled from a backend
-# one would simply substitute the broker with whatever the user wants...
-
-if not engine.startswith("postgre"):
-    raise ConfigurationError("Only PostgreSQL is currently supported as database engine, you have {}".format(engine))
-broker = (
-    "sqla+postgresql://{AIIDADB_USER}:{AIIDADB_PASS}@"
-    "{AIIDADB_HOST}:{AIIDADB_PORT}/{AIIDADB_NAME}"
-).format(**config)
+broker = 'amqp://aiidauser:aiidapass123@localhost:5672/aiidavhost'
 
 app = Celery('tasks', broker=broker)
-
 
 # the tasks as taken from the djsite.db.tasks, same tasks and same functionalities
 # will now of course fail because set_daemon_timestep has not be implementd for SA
