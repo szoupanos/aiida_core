f00af0817c79b7ae4a4880266e91fa0ec829c8a7
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2017-10-27 18:35:25 +0200

    A simple example that works.

--- aiida/cmdline/commands/share.py
+++ aiida/cmdline/commands/share.py
@@ -19,33 +19,16 @@ class Share(VerdiCommandWithSubcommands):
     Share part of your AiiDA graph with other AiiDA instances
     """
 
-    # def __init__(self):
-    #     self.valid_subcommands = {
-    #         'sub_com_a': (self.cli, self.complete_none),
-    #         'main_command': (self.cli, self.complete_none)
-    #     }
+    def __init__(self):
+        self.valid_subcommands = {
+            'sub_com_a': (self.cli, self.complete_none),
+            'user': (self.cli, self.complete_none)
+        }
 
     def cli(self, *args):
         verdi()
 
 # @share.command('name_of_subcommand', context_settings=CONTEXT_SETTINGS)
-
-
-@click.group()
-@click.option('--repo-home', envvar='REPO_HOME', default='.repo')
-@click.option('--debug/--no-debug', default=False,
-              envvar='REPO_DEBUG')
-@click.pass_context
-def cli(ctx, repo_home, debug):
-    ctx.obj = Share(repo_home, debug)
-
-
-
-
-
-
-
-################################### Old commands
 
 @share.command(context_settings=CONTEXT_SETTINGS)
 @click.option('-p', '--past-days', type=int, default=1,
@@ -57,27 +40,9 @@ def cli(ctx, repo_home, debug):
 def sub_com_a(past_days, all_nodes, limit):
     pass
 
-@click.group(context_settings=CONTEXT_SETTINGS)
-def main_command():
-    pass
-
-@main_command.command()
-def foo():
-    click.echo('Initialized the database')
-
-# @cli.group()
-# @click.pass_context
-# def cloudflare(ctx):
-#     pass
 
 @share.command(context_settings=CONTEXT_SETTINGS)
 def user():
     pass
 
-
-
-
-@share.command(context_settings=CONTEXT_SETTINGS)
-def user():
-    pass
 
