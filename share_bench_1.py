0e319bde5713dfb8ba3935ebe3aac90f5ee5eca8
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2017-11-22 13:06:00 +0100

    Adding UUID benchmark (needs to be complited)

---
+++ share_bench_1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+
+from aiida.orm.querybuilder import QueryBuilder
+from aiida.orm.node import Node
+
+qb = QueryBuilder(limit=100)
+qb.append(Node, project=['UUID'])
+res = qb.all()
+
+print res
