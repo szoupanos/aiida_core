7e29922e97017a3be3e35e11ec6c799666692760
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2017-11-23 14:50:33 +0100

    Small changes to the bench scritp

--- share_bench_1.py
+++ share_bench_1.py
@@ -13,14 +13,14 @@ from aiida.orm.node import Node
 import time
 
 # Repeat the following experiment for various UUID sizes
-for lim in [100, 10000, 1000000]:
+for lim in [100, 1000, 10000, 100000, 1000000]:
     print "<================== Round with limit", lim, "==================>"
     # Get some UUIDs -  This represents the set  of nodes that we
     # plan to send to the receiver
     start_q1 = time.time()
-    qb = QueryBuilder(limit=100)
+    qb = QueryBuilder(limit=lim)
     qb.append(Node, project=['uuid'])
-    print "Got", qb.count(), "results \n"
+    print "Got", qb.count(), "results"
     res = [_[0] for _ in qb.all()]
     end_q1 = time.time()
     print "==> Elapsed time for export query (secs)", end_q1 - start_q1
@@ -28,27 +28,33 @@ for lim in [100, 10000, 1000000]:
     # Store the UUIds in a file (emulate that they were send over the network
     print "Storing UUIDs to the file"
     start_s1 = time.time()
+    count_s1 = 0
     with open('tmp_uuid_file.txt', 'wb') as output_file:
         for r in res:
             output_file.write(str(r)+"\n")
+            count_s1 += 1
     end_s1 = time.time()
     print "Finished writing to file"
+    print "Stored", count_s1, "number of lines"
     print "==> Elapsed time for writing to file (secs)", end_s1 - start_s1
 
     # Retrieve the UUIDs from the file
     print "Reading UUIDs from file"
     start_s2 = time.time()
     obtained_uuids = list()
+    count_s2 = 0
     with open('tmp_uuid_file.txt', 'rb') as input_file:
         for line in input_file:
             # obtained_uuids.append(UUID(line[:-1]))
             obtained_uuids.append(line[:-1])
+            count_s2 += 1
     end_s2 = time.time()
+    print "Read", count_s2, "number of lines"
     print "==> Elapsed time for reading from file (secs)", end_s2 - start_s2
 
     # Check which the UUIDs exist in the database and get the ones that
     start_q2 = time.time()
-    qb = QueryBuilder(limit=100)
+    qb = QueryBuilder()
     qb.append(Node, filters={'uuid': {'in': obtained_uuids}}, project=['uuid'])
     existing_uuids = [str(_[0]) for _ in qb.all()]
     end_q2 = time.time()
@@ -60,6 +66,6 @@ for lim in [100, 10000, 1000000]:
     # print "existing_uuids " + str(existing_uuids)
 
     intersect  = list(set(obtained_uuids) - set(existing_uuids))
-    print "Not found UUIDs " + str(intersect)
+    print "Not found UUIDs :" + str(len(intersect))
 
 
