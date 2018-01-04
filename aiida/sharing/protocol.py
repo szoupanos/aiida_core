8c99ce7baa862cebfe77fae84a63cbe155d6cc7e
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2018-01-04 15:27:43 +0100

    More progress on the Protocol & Command classes

--- aiida/sharing/protocol.py
+++ aiida/sharing/protocol.py
@@ -12,9 +12,18 @@ import logging
 
 class Protocol:
 
+    # The buffer size
     BUFFER_SIZE = 1024
+
+    # This is the reserved number of bytes for chunk message
+    # that will be sent by the sender to the receiver
     BYTES_FOR_CHUNK_SIZE_MSG = 10
+
+    # Acknowledgement message
     OK_MSG = 'OK'
+
+    # Channel information needed for the command execution
+    channel = None
 
     def __init__(self):
         pass
@@ -156,8 +165,8 @@ class Protocol:
 
 class Command:
 
-    # Channel information needed for the command execution
-    channel = None
+    # The underlying protocol to be used for the communication
+    protocol = None
 
     # Selected command
     command = None
@@ -169,30 +178,66 @@ class Command:
     # Set of commands
     AVAILABLE_CMDS = set(SEND_FILE, SEND_BUFF)
 
-    def __init__(self, giv_chan, giv_cmd):
-        self.channel = giv_chan
-        self.command = giv_cmd
+    def __init__(self, channel, command, protocol):
+        from aiida.common.exceptions import InvalidOperation
 
-    def submit(self, command):
+        if channel is None or command is None:
+            logging.debug("[Command] " + "You must provide a command and a "
+                                         "channel, exiting")
+            raise InvalidOperation("You must provide a command and a channel.")
+        if command not in self.AVAILABLE_CMDS:
+            logging.debug("[Command] " + "The command requested is not "
+                                         "supported, exiting")
+            raise InvalidOperation("The command requested is not supported.")
+
+        self.channel = channel
+        self.command = command
+
+    def submit(self, command, **kwargs):
         """
         This is the general command
         :param channel:
         :param command:
         :return:
         """
-        if command not in self.AVAILABLE_CMDS:
-            logging.debug("[submit_cmd] " + "Not a valid command, exiting")
-            return 1
-
         # Inform the receiver about the command to be executed
-        self.send(self.channel, command, size_of_chunck = len(command))
+        self.send(self.channel, self.command, size_of_chunck = len(command))
 
         # Execute the command
+        self.send_cmd_selector(**kwargs)
 
-    def send_file_cmd(self):
-        pass
+    def send_cmd_selector(self, argument):
+        switcher = {
+            self.SEND_FILE : self.send_file_cmd,
+            self.SEND_BUFF: self.send_buff_cmd,
+        }
+        # Get the right send command
+        send_cmd = switcher.get(argument)
+        # Execute the method
+        return send_cmd()
 
-    def send_buff_cmd(self):
+    def send_file_cmd(self, filename):
+        # Proceeding to the file sent
+        t = time.time()
+        bytes = 0
+        try:
+            f = open(filename, "rb")
+            while True:
+                chunk = f.read(1024)
+                if not chunk:
+                    break
+
+                bytes += sys.getsizeof(chunk)
+                logging.debug("[send_file_cmd] " + "Sending: " + chunk)
+                byte_no = session_channel.send(chunk)
+                logging.debug("[send_file_cmd] " + "Sent " + str(byte_no)
+                              + " bytes.")
+        finally:
+            logging.debug(
+                "[send_file_cmd] " + "Sending finished, closing file")
+            f.close()
+
+    def send_buff_cmd(self, **kwargs):
         pass
 
 
