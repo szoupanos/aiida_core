e85ca84c2a8b57c77cf960813a80f3633d7c9d10
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2019-04-20 14:30:43 +0000

    More cleaning at the migration

--- aiida/backends/djsite/db/migrations/0034_attributes_extras_json.py
+++ aiida/backends/djsite/db/migrations/0034_attributes_extras_json.py
@@ -11,18 +11,25 @@
 """Adding JSONB field for Node.attributes and Node.Extras"""
 from __future__ import absolute_import
 
-import gc
+from __future__ import print_function
 import math
+import sys
 
 # Remove when https://github.com/PyCQA/pylint/issues/1931 is fixed
 # pylint: disable=no-name-in-module,import-error
+import click
 from django.db import transaction
 import django.contrib.postgres.fields.jsonb
 from django.db import migrations
 from aiida.backends.djsite.db.migrations import upgrade_schema_version
+from aiida.backends.utils import datetime_to_isoformat
+from aiida.cmdline.utils import echo
 
 REVISION = '1.0.34'
 DOWN_REVISION = '1.0.33'
+
+# Nodes are processes in groups of the following size
+group_size = 1000
 
 
 def lazy_bulk_fetch(max_obj, max_count, fetch_func, start=0):
@@ -32,90 +39,47 @@ def lazy_bulk_fetch(max_obj, max_count, fetch_func, start=0):
         counter += max_obj
 
 
-# def transition_attributes(profile=None, group_size=1000, debug=False, delete_table=False):
-def transition_attributes(apps, _):
+def transition_attributes_extras(apps, _):
     """
     Migrate the DbAttribute table into the attributes column of db_dbnode.
     """
-    print("\nStarting migration of attributes")
-    # node_table_cols = inspector.get_columns(NODE_TABLE_NAME)
+    db_node_model = apps.get_model('db', 'DbNode')
+    echo.echo_warning('Starting the data migration of attributes and extras.')
 
-    DbNode = apps.get_model('db', 'DbNode')
-    DbAttributes = apps.get_model('db', 'DbAttribute')
-
-    group_size = 1000
-
-    # session = sa.get_scoped_session()
-
-    # with session.begin(subtransactions=True):
     with transaction.atomic():
+        total_node_no = db_node_model.objects.count()
 
-        # from aiida.backends.sqlalchemy.models.node import DbNode
-        # total_nodes = session.query(func.count(DbNode.id)).scalar()
-        total_nodes = DbNode.objects.count()
+        if total_node_no == 0:
+            return
 
-        fetcher = lazy_bulk_fetch(50, DbNode.objects.count(), DbNode.objects.all)
-        error = False
+        with click.progressbar(label='Modifying user accounts',
+                               length=total_node_no, show_pos=True) as bar:
+            fetcher = lazy_bulk_fetch(group_size, total_node_no, db_node_model.objects.all)
+            error = False
 
-        for batch in fetcher:
-            # Get the ids of the nodes
-            print(batch)
-            updated_nodes = list()
-            for curr_dbnode in batch:
-                print("node ==========>" + str(curr_dbnode))
-                print("dbattr ==========>" + str(curr_dbnode.dbattributes.all()))
-                print("attr ==========>" + str(curr_dbnode.attributes))
+            for batch in fetcher:
+                for curr_dbnode in batch:
 
-                # Migrating attributes
-                dbattrs = list(curr_dbnode.dbattributes.all())
-                attrs, err_ = attributes_to_dict(sorted(dbattrs, key=lambda a: a.key))
-                error |= err_
-                curr_dbnode.attributes = attrs
+                    # Migrating attributes
+                    dbattrs = list(curr_dbnode.dbattributes.all())
+                    attrs, err_ = attributes_to_dict(sorted(dbattrs, key=lambda a: a.key))
+                    error |= err_
+                    curr_dbnode.attributes = datetime_to_isoformat(attrs)
 
-                # Migrating extras
-                dbextr = list(curr_dbnode.dbextras.all())
-                extr, err_ = attributes_to_dict(sorted(dbextr, key=lambda a: a.key))
-                error |= err_
-                curr_dbnode.extras = extr
+                    # Migrating extras
+                    dbextr = list(curr_dbnode.dbextras.all())
+                    extr, err_ = attributes_to_dict(sorted(dbextr, key=lambda a: a.key))
+                    error |= err_
+                    curr_dbnode.extras = datetime_to_isoformat(extr)
 
-                # Saving the result
-                curr_dbnode.save()
+                    # Saving the result
+                    curr_dbnode.save()
+                    bar.update(1)
 
-                print("WWWWWWW 1 ===>", str(curr_dbnode.attributes))
-                print("WWWWWWW 2 ===>", str(curr_dbnode.extras))
+                    if error:
+                        raise Exception("There has been some errors during the migration")
 
-            # DbNode.objects.bulk_create()
-
-
-        # total_groups = int(math.ceil(total_nodes / group_size))
-        # error = False
-        #
-        # for i in range(total_groups):
-        #     print("Migrating group {} of {}".format(i, total_groups))
-        #
-        #     node_ids = DbNode.objects.all()
-        #
-        #     nodes = DbNode.query.options(
-        #         subqueryload('old_attrs'), load_only('id', 'attributes')
-        #     ).order_by(DbNode.id)[i * group_size:(i + 1) * group_size]
-        #
-        #     for node in nodes:
-        #         attrs, err_ = attributes_to_dict(sorted(node.old_attrs,
-        #                                                 key=lambda a: a.key))
-        #         error |= err_
-        #
-        #         node.attributes = attrs
-        #         session.add(node)
-        #
-        #     # Remove the db_dbnode from sqlalchemy, to allow the GC to do its
-        #     # job.
-        #     session.flush()
-        #     session.expunge_all()
-        #
-        #     del nodes
-        #     gc.collect()
-    print("Migration of attributes finished.")
-
+    echo.echo_warning('Finished the data migration of attributes and extras.')
 
 def attributes_to_dict(attr_list):
     """
@@ -129,8 +93,8 @@ def attributes_to_dict(attr_list):
         try:
             tmp_d = select_from_key(a.key, d)
         except Exception:
-            print("Couldn't transfer attribute {} with key {} for dbnode {}"
-                  .format(a.id, a.key, a.dbnode_id))
+            print(("Couldn't transfer attribute {} with key {} for dbnode {}"
+                  .format(a.id, a.key, a.dbnode_id)))
             error = True
             continue
         key = a.key.split('.')[-1]
@@ -161,7 +125,7 @@ def attributes_to_dict(attr_list):
 
             tmp_d[key] = val
 
-    return (d, error)
+    return d, error
 
 
 def select_from_key(key, d):
@@ -179,6 +143,7 @@ def select_from_key(key, d):
             tmp_d = tmp_d[p]
 
     return tmp_d
+
 
 class Migration(migrations.Migration):
     """
@@ -207,10 +172,8 @@ class Migration(migrations.Migration):
             name='extras',
             field=django.contrib.postgres.fields.jsonb.JSONField(default=None, null=True),
         ),
-        # ##############################################################
         # Migrate the data from the DbAttribute table to the JSONB field
-        # ##############################################################
-        migrations.RunPython(transition_attributes, reverse_code=migrations.RunPython.noop),
+        migrations.RunPython(transition_attributes_extras, reverse_code=migrations.RunPython.noop),
 
         # Delete the binding of Node-Attribute
         migrations.AlterUniqueTogether(
@@ -239,17 +202,4 @@ class Migration(migrations.Migration):
         migrations.DeleteModel(name='DbExtra', ),
 
         upgrade_schema_version(REVISION, DOWN_REVISION),
-
-        # The following is not needed because it was done by Sebastiaan too in migration 0033
-        # I don't see any data migration for 00033. Is it done automatically?
-
-        # # Change to DbComputer.transport_params and DbComputer.metadata to JSONB
-        # migrations.AlterField(
-        #     model_name='dbcomputer',
-        #     name='transport_params',
-        #     field=django.contrib.postgres.fields.jsonb.JSONField(default={}, null=True)),
-        # migrations.AlterField(
-        #     model_name='dbcomputer',
-        #     name='metadata',
-        #     field=django.contrib.postgres.fields.jsonb.JSONField(default={}, null=True)),
     ]
